b5ee9e91332595cca46d86c490bae3173055c519
Stephen Martin <stephen.martin.email@gmail.com>
2016-04-19 22:23:40 +1000

    qr maker done

--- qr_quote.py
+++ qr_quote.py
@@ -1,16 +1,26 @@
 import qrcode
 import click
+from urllib.parse import quote
 
 @click.command()
 @click.argument('amount')
-@click.option('-m', '--message', default="bitcoin payment")
+@click.option('-m', '--message', type=str, default='bitcoin payment')
 
 
 
-def generate_quote(amount, message):
-	address = "1ZUWgvPfNskFv3CeWUfE4QRHwTVsoiu9o"
-	encode_string = 'bitcoin:' + address + '?amount=' + amount + '&message=' + message
-	print(message)
+def generate_quote(message, amount):
+  address = "1ZUWgvPfNskFv3CeWUfE4QRHwTVsoiu9o"
+  encode_string = 'bitcoin:' + address + '?amount=' +  amount + '&message=' + quote(message, safe='')
+  print(encode_string)
+  qr = qrcode.QRCode(
+        version=5,  #version 3 is smallest size bitcoin address will compress to
+        error_correction=qrcode.constants.ERROR_CORRECT_M,
+        border=4,
+    )
+  qr.add_data(encode_string)
+  qr.make()
+  img = qr.make_image()
+  img.save("qr_view.png")
 
 if __name__ == '__main__':
 	generate_quote()
